425067235d3a1975988baf79dab7ca3b7f2e1ac0
rhf0220 <allenfrh@gmail.com>
2025-02-13 20:32:40 -0500

    Add Web employee.sql
    As a sql command, insert a new employee whose ID is 999999 first and last names are WEB for all this made online

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,11 +1,8 @@
-
 CREATE TABLE IF NOT EXISTS Authentication (
     UserID TEXT PRIMARY KEY,
     PasswordHash TEXT NOT NULL,
     SessionID TEXT
 );
-
-
 
 CREATE TABLE IF NOT EXISTS Shopping_cart (
     ShopperID TEXT PRIMARY KEY,
@@ -14,3 +11,8 @@ CREATE TABLE IF NOT EXISTS Shopping_cart (
     AddedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
 );
+
+-- Adds a new employee whose ID is 999999, first and last names are "WEB"
+-- This employee is used to represent the web application itself
+INSERT INTO Employees (EmployeeID, LastName, FirstName)
+VALUES (999999, "WEB", "WEB");
