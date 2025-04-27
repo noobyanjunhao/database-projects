627cf838e2799a8a4cd15965c1f3de9545415ff4
YuqiWang1106 <wangebb@bc.edu>
2025-04-27 15:20:18 -0400

    Database

--- PA3/flaskr/db.py
+++ PA3/flaskr/db.py
@@ -18,15 +18,22 @@ def close_db(e=None):
     if db is not None:
         db.close()
 
+# def init_db():
+#     db = get_db()
+#     with current_app.open_resource('schema.sql') as f:
+#         db.executescript(f.read().decode('utf8'))
+
 def init_db():
     db = get_db()
-    with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
+    with current_app.open_resource("schema.sql") as f:
+        db.executescript(f.read().decode("utf8"))
+    with current_app.open_resource("seed.sql") as f:
+        db.executescript(f.read().decode("utf8"))
 
 @click.command('init-db')
 @with_appcontext
 def init_db_command():
-    """CLI: 初始化数据库"""
+    """CLI: Initialize the Database"""
     init_db()
     click.echo('Initialized the database.')
 
